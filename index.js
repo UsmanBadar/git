3e330795595654049bba40146d973e1ae6f20a41
UsmanBadar <usman.badar006@gmail.com>
2021-03-07 00:08:09 +1100

    changes made to index.html

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = ()=> {
+ return 3 + 3;
+};
+
+const chesse = ()=> {
+    return 'rat likes chesse';
+};
+let mn = 6;
+let array = [3, 5, 6];
+
+array.push(8);
+console.log(array);
+
+let numberFunction = ()=> {
+
+    console.log(array);
+};
+
+const tuple = ()=> {
+    number * number;
+};
+
+array.map((array)=> {
+    console.log(array);
+});
